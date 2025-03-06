a8abce58817e8db6a11ccdaead36d10d1903e6a5
lyxffq <lyxffq@mail.ru>
2025-03-06 11:56:49 +0500

    убрал повторяющиеся блоки

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import ParametrBlock from './components/ParametrBlock/ParametrBlock'
-import ParametrHead from './components/ParametrHead/ParametrHead'
 import ParametrItem from './components/ParametrItem/ParametrItem'
 
 function App() {
 
   return (
     <>
-      <ParametrHead name='Параметр' value='Величина' unit='Ед. изм.' />
+      <ParametrItem name='Параметр' value='Величина' unit='Ед. изм.'isHead='yes' />
       <ParametrBlock name='Параметры нефти' />
       <ParametrItem name='Уровень нефти (факт)' value='8.34' unit='м' />
       <ParametrItem name='Расход газа' value='1446.484' unit='тыс.нм^3/сут' />
--- src/components/ParametrItem/ParametrItem.jsx
+++ src/components/ParametrItem/ParametrItem.jsx
@@ -1,7 +1,15 @@
 import './ParametrItem.css';
 
-export default function ParametrItem({ name, value, unit }) {
-
+export default function ParametrItem({ name, value, unit, isHead }) {
+    if (isHead) {
+        return (
+            <div className="parametr-head">
+                <div className="parametr-head__name">{ name }</div>
+                <div className="parametr-item__value">{ value }</div>
+                <div className="parametr-item__unit">{ unit }</div>
+            </div>
+        );
+    }
     return (
         <div className="parametr-item">
             <div className="parametr-item__name">{ name }</div>
