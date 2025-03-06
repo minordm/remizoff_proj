a8e1e48bf73cd366b990a3058cd471463d2d28ca
lyxffq <lyxffq@mail.ru>
2025-03-06 15:06:00 +0500

    изменил таблички в svg-схеме, добавил счетчик по клику

--- src/layout/Main/BodyScheme/BodyScheme.jsx
+++ src/layout/Main/BodyScheme/BodyScheme.jsx
@@ -1,11 +1,23 @@
 import "./BodyScheme.css";
 import { ReactSVG } from "react-svg";
+import { useState } from 'react';
 
 export default function BodyScheme() {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="body-scheme">
-      <ReactSVG src="./scheme2-01-1.svg" className='scheme1' />
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <ReactSVG
+        src="./scheme2-01-1.svg"
+        className="scheme1"
+        beforeInjection={(svg) => {
+          const textElement = svg.querySelector("#oilСonsumption");
+          if (textElement) {
+            textElement.textContent = count;
+          }
+        }}
+      />
     </div>
   );
 }
