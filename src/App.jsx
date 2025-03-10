06a9d4b3299fe1709eae455c54199df1393d20cb
minor.dm <lugovoi21@gmail.com>
2025-03-10 08:00:09 +0500

    добавил переход по страницам и страницу с ошибкой

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import './App.css'
-// import ParametrList from "./components/ParametrList/ParametrList.jsx";
-// import Barrel from "./components/Barrel/Barrel.jsx";
-// import LeftPanel from "./layout/LeftPanel/LeftPanel.jsx";
-// import Body from "./layout/Body/Body.jsx";
-// import AlertPanel from "./components/AlertPanel/AlertPanel.jsx";
-// import { LineGraph } from './components/Graphics/Line.jsx';
+import ParametrList from "./components/Graphic/ParametrList/ParametrList.jsx";
+import Barrel from "./components/Graphic/Barrel/Barrel.jsx";
+import LeftPanel from "./layout/Graphic/LeftPanel/LeftPanel.jsx";
+import Body from "./layout/Graphic/Body/Body.jsx";
+import AlertPanel from "./components/Graphic/AlertPanel/AlertPanel.jsx";
+import { LineGraph } from "./components/Graphic/Graphics/Line.jsx";
 import BodyScheme from "./layout/Main/BodyScheme/BodyScheme";
 import RightPanel from "./layout/Main/RightPanel/RightPanel";
 import Diagrama from "./components/Main/Diagrama/Diagrama";
 import AlertList from "./components/Main/AlertList/AlertList";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from './components/Error/Error.jsx';
 
-function App() {
-
-  return (
-    <div className="app">
-      {/* <LeftPanel>
-            <ParametrList />
-            <Barrel />
-        </LeftPanel>
-        <Body>
-            <LineGraph />
-            <AlertPanel />
-        </Body> */}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
       <div className="main-page">
         <BodyScheme />
         <RightPanel>
@@ -29,6 +23,46 @@ function App() {
           <AlertList />
         </RightPanel>
       </div>
+    ),
+  },
+  {
+    path: "/graphic",
+    element: (
+      <div className='main-page'>
+        <LeftPanel>
+          <ParametrList />
+          <Barrel />
+        </LeftPanel>
+        <Body>
+          <LineGraph />
+          <AlertPanel />
+        </Body>
+      </div>
+    ),
+  },
+  {
+    path: '*',
+    element: (
+      <div>
+        <Error />
+      </div>
+    )
+  }
+]);
+
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <div>
+        <a href='/'>Main</a>
+        <a href='/graphic'>Graphic</a>
+      </div>
+
+      <RouterProvider router={router} />
+      
     </div>
   );
 }
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <>
+            <div>Это фиаско, братан</div>
+            <img src='./1.jpg' alt='err' />
+        </>
+    )
+}
